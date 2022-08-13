90e8b3de60784c3a4f5a03001969880154f7744e
sadeq460 <msadeqrezaei@gmail.com>
2022-08-13 22:48:04 +0430

    add URL parameters (fix bug)

--- codes/main.js
+++ codes/main.js
@@ -101,7 +101,8 @@ function showinfo(number){
     document.getElementById("center").innerHTML =  video;
     document.getElementById("name_text").innerHTML = title; 
 
-    window.history.replaceState('', '', 'http://localhost/sadeqam/sadeqam/' + course + "/" + id);
+    //for lacal http://localhost/sadeqam/sadeqam/
+    window.history.replaceState('', '', 'https://sadeqam.ir/' + course + "/" + id);
 }
 
 function getAparatVideo(number){
