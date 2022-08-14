3355e1d4a4c22cb50954b1ccb135ff545ead7117
sadeq460 <msadeqrezaei@gmail.com>
2022-08-14 22:21:54 +0430

    redirect to index keep url

--- codes/main.js
+++ codes/main.js
@@ -1,16 +1,5 @@
 import data from './data/data.json' assert {type: 'json'};
 
-// if it's on index.html
-if (window.sessionStorage.path) {
-    let path = window.sessionStorage.path; // gets the path saved on sessionStorage
-    window.history.pushState(null, null, path); // push it on url
-    window.sessionStorage.removeItem('path'); // removes from session
-} else { // if it's on 404.html (path doens't exist)
-    let path = window.location.pathname; // get the path url
-    window.sessionStorage.path = path; // saves on sessionStorage
-    window.location.href = '/'; // go to index
-}
-
 var number = 0;
 var course = "java";
 showinfo(number);
